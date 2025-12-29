685404baaf93ae593d33d1bcf049c5c8166bc86c
ViplavKhode <yesviplavkhode@gmail.com>
2025-12-28 21:29:03 -0500

    feat: Implement map UI using Leaflet map.

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,24 @@
 const socket = io();
+
+//from documentation
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+        (position) => {
+            const { latitude, longitude } = position.coords;
+            socket.emit("send-location", { latitude, longitude });
+        },
+        (error) => {
+            console.log(error);
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        });
+}
+
+const map = L.map("map").setView([0, 0], 10);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: 'Spot-On by Viplav Khode'
+}).addTo(map);
